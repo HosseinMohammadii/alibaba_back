7bc45b6fd09bfdc13cf93c65429ac620808365f6
hossein_7 <hossein.slayer@gmail.com>
2020-08-12 01:49:16 +0430

    cors fixed

--- alibaba/settings.py
+++ alibaba/settings.py
@@ -52,8 +52,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
-    'middlewares.middlewares.CORSMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    # 'middlewares.middlewares.CORSMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
